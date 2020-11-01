3ca96931c088465ad2e3263998bce98afce70f61
Natasha <natu.rios@hotmail.com>
2020-10-31 21:19:06 -0300

    article

--- pages/articulo/[id].js
+++ pages/articulo/[id].js
@@ -1,15 +1,24 @@
-export default function DynamicRoute({ title, tags, content }) {
-    return (
-    <div>
-      <h1>{title}</h1>
-      <ul>
+import styles from '../../styles/Article.module.scss';
+
+export default function DynamicRoute({ title, tags, content, coverImage, user, date }) {
+  console.log(user)
+  return (
+    <div className={styles.container}>
+      <img className={styles.coverImage} src={coverImage} />
+      <h1 className={styles.title}>{title}</h1>
+      <ul className={styles.ul}>
         {tags.map((tag, key) => {
             return(
-                <li key={key}>{tag}</li>
+                <li className={styles.li} key={key}>#{tag}</li>
             )
         })}
       </ul>
-      <p dangerouslySetInnerHTML={{__html: content}}></p>
+      <div className={styles.user}>
+         <img className={styles.userImg} src={user.profile_image} />
+        <p className={styles.userName}>{user.name}</p> 
+        <p className={styles.date}>{date}</p>
+      </div>
+      <p className={styles.text} dangerouslySetInnerHTML={{__html: content}}></p>
     </div>
     )
   }
@@ -39,7 +48,10 @@ export default function DynamicRoute({ title, tags, content }) {
       props: {
         title: json.title,
         tags: json.tags,
-        content: json.body_markdown
+        content: json.body_markdown,
+        coverImage: json.cover_image,
+        user: json.user,
+        date: json.readable_publish_date
       },
       revalidate: 3600
     };
